d6d1548bd7dcf40331f516e71bac88fd68410230
Dan <dchen12812@gmail.com>
2015-09-23 22:46:55 -0700

    changed to javascript floor and string convertsion as well as semicolons

--- scripts/stopwatch.js
+++ scripts/stopwatch.js
@@ -1,9 +1,9 @@
 
 // define global variables
-var cTime = 0
-var x = 0 //successful stop
-var y = 0 //total stop
-var running = true
+var cTime = 0;
+var x = 0; //successful stop
+var y = 0; //total stop
+var running = true;
 
 //Selects the canvas
 var c = document.getElementById("myCanvas");
@@ -11,37 +11,36 @@ var ctx = c.getContext("2d");
 
 
 // define helper function format that converts time
-// in tenths of seconds into formatted string A:BC.D
+// in tenths of seconds into formatted Stringing A:BC.D
 function format(t){  
-    var d = t % 10
-    var c = ((t - d) % 100) /10
-    var b = (t / 100) % 6
-    var a = int(t/600)
-    return(str(a) + ':' + str(b) + str(c) + '.' + str(d))
+    var d = t % 10;
+    var c = ((t - d) % 100) /10;
+    var b = (t / 100) % 6;
+    var a = Math.floor(t/600);
+    return(String(a) + ':' + String(b) + String(c) + '.' + String(d));
 }
     
 // define event handlers for buttons; "Start", "Stop", "Reset"
 function start_handler(){   
-    running = true
-    timer = setInterval(function(){timer_handler()}, 100)
+    running = true;
+    timer = setInterval(function(){timer_handler()}, 100);
 }
 
 function stop_handler(){
     if(running == true){
-
         if(cTime %10 == 0){
-            x+= 1
+            x+= 1;
         }
-        y += 1
-        running = false
+        y += 1;
+        running = false;
     }
     clearInterval(timer);
 }
 
 function reset_handler(){
-    cTime = 0
-    x = 0
-    y = 0
+    cTime = 0;
+    x = 0;
+    y = 0;
     clearInterval(timer);
 }
 
@@ -52,17 +51,18 @@ function timer_handler(){
 
 // define draw handler
 function draw_handler(canvas){
-    canvas.draw_text(format(cTime),(180,100),20,"white")
-    canvas.draw_text('Success: '+ str(x),(280,40),20,"white")
-    canvas.draw_text('Total: ' + str(y),(280,20),20,"white")
+    canvas.fillStyle = "white";
+    canvas.font = '20px Comic Sans MS';
+    canvas.fill_text(format(cTime), 180,100);
+    canvas.fill_text('Success: '+ String(x),280,40);
+    canvas.fill_text('Total: ' + String(y),280,20);
 }
         
 // create frame
-frame = simplegui.create_frame("Stopwatch: The Game", 400, 200)
-var timer = setInterval(function(){timer_handler()}, 100)
+var timer = setInterval(function(){timer_handler()}, 100);
 
 // register event handlers
-frame.set_draw_handler(draw_handler)
+draw_handler(ctx);
 
 // start frame
 frame.start()
